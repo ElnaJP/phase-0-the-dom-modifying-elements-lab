a6560bb116473f9bea47b14ccc35a9b0f2d286b6
Elna JP <eriosdd@gmail.com>
2022-07-29 11:53:52 -0400

    Assignment completed

--- index.js
+++ index.js
@@ -10,38 +10,30 @@ for (let i = 0; i < 3; i++) {
 element.append(ul);
 console.log(element.append(ul));
 
-//someElement.removeChild(someChildElement);
-//const secondChild = ul.querySelector("li:nth-child(2)");
-//ul.removeChild(secondChild);
+someElement.removeChild(someChildElement);
+const secondChild = ul.querySelector("li:nth-child(2)");
+ul.removeChild(secondChild);
 
-//li.textContent = "Hi there!";
-// => <li>Hi there!</li>
-//console.log(li.textContent);
-// => "Hi there!"*/
+li.textContent = "Hi there!";
 
+console.log(li.textContent);
 
 
 
 
-
-
-
-
-
-
-/*const main = document.getElementById("main");
+const main = document.getElementById("main");
 main.innerHTML =
   "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
 
-/*element.style.height = "60px";
+element.style.height = "60px";
 element.style.backgroundColor = "#27647B";
 element.textContent = "Come on Elna, you can do this!";
 element.style.fontSize = "20px";
 element.style.marginLeft = "20px";
-element.style.lineHeight = 2;*/
+element.style.lineHeight = 2;
 
-/*main.remove('main');
-/*element.className = "pet-listing dog";
+main.remove('main');
+element.className = "pet-listing dog";
 element.classList.remove("dog");
 element.classList.add("cat", "sale");*/
 
@@ -50,18 +42,9 @@ element.classList.add("cat", "sale");*/
 
 
 
-
-
-
-let newHeader = document.createElement('h1');
-newHeader.id = 'victory';
-newHeader.textContent = "Elna is the champion";
-document.body.appendChild(newHeader);
-
-
-
-
-
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Elna is the champion";
 
 
 
